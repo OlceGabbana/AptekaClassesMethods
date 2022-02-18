9e834667c399a39a66fa7116b4e76135ab5ddb82
Оля Соколова <Olya.Sokolova.241902@yandex.ru>
2022-02-18 09:02:54 +0300

    Добавьте файлы проекта.

---
+++ AptekaClassesMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AptekaClassesMethods
+{
+    class Chemist
+    {
+        // Название аптеки
+        public string name;
+        // Названия лекарств
+        public string name_med;
+        // стоимость лекарств
+        public int money;
+
+        public void Initialization(string name, string name_med, int money)
+        {
+            Chemist chemist1 = new Chemist();
+            chemist1.name = name;
+            chemist1.name_med = name_med;
+            chemist1.money = money;
+
+        }
+    }
+    
+}
